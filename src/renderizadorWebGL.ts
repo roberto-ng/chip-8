440113ae3275aa1a3546a48f5d8b8db40311b343
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2019-02-01 02:43:48 -0200

    Ajustes na classe RenderizadorWebGL

--- src/renderizadorWebGL.ts
+++ src/renderizadorWebGL.ts
@@ -7,30 +7,51 @@
 import IRenderizador from './renderizador';
 
 export default class RenderizadorWebGL implements IRenderizador {
-    private _gl: WebGLRenderingContext;
-    private _largura: number;
-    private _altura: number;
-    private _shader: WebGLProgram;
-    private _texCoords: number[];
-    private _coordBuffer: WebGLBuffer;
-    private _posicaoBuffer: WebGLBuffer;
-    private _indicesBuffer: WebGLBuffer;
-    private u_sampler: WebGLUniformLocation;
-    private a_textCoord: number;
+    private gl: WebGLRenderingContext;
+    private shader: WebGLProgram;
+    private tex_coords: number[];
+    private coord_buffer: WebGLBuffer;
+    private pos_buffer: WebGLBuffer;
+    private indices_buffer: WebGLBuffer;
+    private u_textura: WebGLUniformLocation;
+    private a_tex_coord: number;
     private a_posicao;
     private textura: WebGLTexture;
 
     public readonly PIXEL_TAMANHO: number = 8;
+    public readonly LARGURA: number = 64;
+    public readonly ALTURA: number = 32;
     
     public constructor(gl: WebGLRenderingContext) {
-        console.log('usando webgl');
-        this._gl = gl;
-        this._largura = this.PIXEL_TAMANHO * 64;
-        this._altura = this.PIXEL_TAMANHO * 32;
-        //this._cor = new Array(4);
-        this._shader = this.compilarShaderUsarTextura();
+        this.gl = gl;
 
-        this._texCoords = [
+        const v_source = `
+            precision mediump float;
+            
+            attribute vec3 a_pos;
+            attribute vec2 a_tex_coord;
+            varying vec2 v_tex_coord;
+
+            void main() {
+                gl_Position = vec4(a_pos, 1.0);
+                v_tex_coord = a_tex_coord;
+            }
+        `;
+
+        const f_source = `
+            precision mediump float;
+
+            uniform sampler2D u_textura;
+            varying vec2 v_tex_coord;
+
+            void main() {
+                gl_FragColor = texture2D(u_textura, v_tex_coord);
+            }
+        `;
+
+        this.shader = this.compilarShader(v_source, f_source);
+
+        this.tex_coords = [
             1.0, 1.0, // cima direita
             1.0, 0.0, // baixo direita
             0.0, 0.0, // baixo esquerda
@@ -49,59 +70,59 @@ export default class RenderizadorWebGL implements IRenderizador {
             1, 2, 3, // segundo triangulo
         ];
 
-        const textureCoordBuffer = this._gl.createBuffer();
+        const textureCoordBuffer = this.gl.createBuffer();
         if (textureCoordBuffer === null) {
             throw new Error('Erro ao criar textureCoordBuffer');
         }
-        this._coordBuffer = textureCoordBuffer;
+        this.coord_buffer = textureCoordBuffer;
 
-        const posicaoBuffer = this._gl.createBuffer();
+        const posicaoBuffer = this.gl.createBuffer();
         if (posicaoBuffer === null) {
             throw new Error('Erro ao criar posicaoBuffer');
         }
-        this._posicaoBuffer = posicaoBuffer;
+        this.pos_buffer = posicaoBuffer;
 
-        const indicesBuffer = this._gl.createBuffer();
+        const indicesBuffer = this.gl.createBuffer();
         if (indicesBuffer === null) {
             throw new Error('Erro ao criar indicesBuffer');
         }
-        this._indicesBuffer = indicesBuffer;
+        this.indices_buffer = indicesBuffer;
         
-        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._posicaoBuffer);
-        this._gl.bufferData(
-            this._gl.ARRAY_BUFFER, 
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pos_buffer);
+        this.gl.bufferData(
+            this.gl.ARRAY_BUFFER, 
             new Float32Array(posicao),
-            this._gl.STATIC_DRAW
+            this.gl.STATIC_DRAW
         );
 
-        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._coordBuffer);
-        this._gl.bufferData(
-            this._gl.ARRAY_BUFFER, 
-            new Float32Array(this._texCoords),
-            this._gl.STATIC_DRAW
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.coord_buffer);
+        this.gl.bufferData(
+            this.gl.ARRAY_BUFFER, 
+            new Float32Array(this.tex_coords),
+            this.gl.STATIC_DRAW
         );
 
-        this._gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._indicesBuffer);
-        this._gl.bufferData(
+        this.gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices_buffer);
+        this.gl.bufferData(
             gl.ELEMENT_ARRAY_BUFFER, 
             new Uint16Array(indices), 
             gl.STATIC_DRAW
         );
 
-        this.a_textCoord = gl.getAttribLocation(this._shader, 'a_tex_coord');
-        this.a_posicao = gl.getAttribLocation(this._shader, 'a_pos');
+        this.a_tex_coord = gl.getAttribLocation(this.shader, 'a_tex_coord');
+        this.a_posicao = gl.getAttribLocation(this.shader, 'a_pos');
         
-        const u_sampler = gl.getUniformLocation(this._shader, 'u_sampler');
-        if (u_sampler === null) { throw new Error('erro ao achar u_sampler'); }
-        this.u_sampler = u_sampler;
+        const u_textura = gl.getUniformLocation(this.shader, 'u_textura');
+        if (u_textura === null) { throw new Error('erro ao achar u_textura'); }
+        this.u_textura = u_textura;
 
-        const textura = this._gl.createTexture();
+        const textura = this.gl.createTexture();
         if (textura === null) {
             throw new Error('Erro ao criar textura');
         }
         this.textura = textura;
 
-        this._gl.viewport(0, 0, this._gl.canvas.width, this._gl.canvas.height);
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
         this.limparTela();
     }
 
@@ -123,8 +144,8 @@ export default class RenderizadorWebGL implements IRenderizador {
     }
 
     public limparTela(): void {
-        this._gl.clearColor(0, 0, 0, 0);
-        this._gl.clear(this._gl.COLOR_BUFFER_BIT);
+        this.gl.clearColor(0, 0, 0, 0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
     }
 
     public desenharQuadrado(x: number, y: number, l: number, a: number): void {
@@ -133,151 +154,116 @@ export default class RenderizadorWebGL implements IRenderizador {
     public encerrarFrame(): void {
     }
 
+    /** Cria e renderiza uma textura baseada no buffer da tela */
     public desenharTela(tela: number[][]): void {
-        this._gl.useProgram(this._shader);
+        this.gl.useProgram(this.shader);
         
-        this._gl.bindTexture(this._gl.TEXTURE_2D, this.textura);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.textura);
         
         //this._gl.pixelStorei(this._gl.UNPACK_ALIGNMENT, 1);
-        this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGBA, 64, 32, 
-            0, this._gl.RGBA, this._gl.UNSIGNED_BYTE, this.criarTextura(tela));
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.LARGURA, this.ALTURA, 
+            0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, this.criarTexturaRGBA(tela));
 
-        this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_MAG_FILTER, this._gl.NEAREST);
-        this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_MIN_FILTER, this._gl.NEAREST);
-        this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_WRAP_S, this._gl.CLAMP_TO_EDGE);
-        this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_WRAP_T, this._gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
         
-        this._gl.enableVertexAttribArray(this.a_textCoord);
-        this._gl.enableVertexAttribArray(this.a_posicao);
+        this.gl.enableVertexAttribArray(this.a_tex_coord);
+        this.gl.enableVertexAttribArray(this.a_posicao);
 
-        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._coordBuffer);
-        this._gl.vertexAttribPointer(this.a_textCoord, 2, this._gl.FLOAT, false, 0, 0);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.coord_buffer);
+        this.gl.vertexAttribPointer(this.a_tex_coord, 2, this.gl.FLOAT, false, 0, 0);
 
-        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._posicaoBuffer);
-        this._gl.vertexAttribPointer(this.a_posicao, 3, this._gl.FLOAT, false, 0, 0);        
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pos_buffer);
+        this.gl.vertexAttribPointer(this.a_posicao, 3, this.gl.FLOAT, false, 0, 0);        
 
-        this._gl.activeTexture(this._gl.TEXTURE0);
-        this._gl.uniform1i(this.u_sampler, 0);
-        this._gl.bindTexture(this._gl.TEXTURE_2D, this.textura);
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.uniform1i(this.u_textura, 0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.textura);
 
-        this._gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        this._gl.clearDepth(1.0);
-        this._gl.enable(this._gl.DEPTH_TEST);
-        this._gl.depthFunc(this._gl.LEQUAL);
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.gl.clearDepth(1.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.depthFunc(this.gl.LEQUAL);
 
-        // limpa o canvas
-        //this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
-
-        this._gl.bindBuffer(this._gl.ELEMENT_ARRAY_BUFFER, this._indicesBuffer);
-        this._gl.drawElements(this._gl.TRIANGLES, 6, this._gl.UNSIGNED_SHORT, 0);
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indices_buffer);
+        this.gl.drawElements(this.gl.TRIANGLES, 6, this.gl.UNSIGNED_SHORT, 0);
     }
 
-    private criarTextura(tela: number[][]): Uint8Array {
+    /** Transforma a tela em uma textura RGBA */
+    private criarTexturaRGBA(tela: number[][]): Uint8Array {
         let textura: number[] = [];
-        let pixels: number[][] = [];
+        for (let y = this.ALTURA-1; y >= 0; --y) {
+            for (let x = 0; x < this.LARGURA; ++x) {
+                const pixel = tela[y][x];
 
-        let count = 0;
-        for (let y = 31; y >= 0; --y) {
-            for (let x = 0; x < 64; ++x) {
                 // cor branca
                 let cor = [255, 255, 255, 255];
-                const pixel = tela[y][x];
                 if (pixel === 0) {
+                    // cor roxa
                     cor = [57, 50, 71, 255];
                 }
         
-               pixels.push(cor);
-            }
-        }
-
-        for (let pixel of pixels) {
-            for (let valor of pixel) {
-                textura.push(valor);
+               for (let valor of cor) {
+                   textura.push(valor);
+               }
             }
         }
 
         return new Uint8Array(textura);
     }
 
-    private compilarShaderUsarTextura(): WebGLProgram {
-        const v_source = `
-            precision mediump float;
-            
-            attribute vec3 a_pos;
-            attribute vec2 a_tex_coord;
-            varying vec2 v_tex_coord;
-
-            void main() {
-                gl_Position = vec4(a_pos, 1.0);
-                v_tex_coord = a_tex_coord;
-            }
-        `;
-
-        const f_source = `
-            precision mediump float;
-
-            uniform sampler2D u_sampler;
-            varying vec2 v_tex_coord;
-
-            void main() {
-                gl_FragColor = texture2D(u_sampler, v_tex_coord);
-            }
-        `;
-
-        return this.compilarPrograma(v_source, f_source);
-    }
-
-    /** Compila o programa que irá rodar na placa de video e que
-     * será usado para desenhar os quadrados
-     * @returns O programa já compilado
+    /** Compila o programa que irá rodar na placa de video para desenhar a textura do buffer da tela
+     * @returns O programa compilado
      */
-    private compilarPrograma(vSource: string, fSource: string): WebGLProgram {
-        const vertexShader = this._gl.createShader(this._gl.VERTEX_SHADER);
+    private compilarShader(vSource: string, fSource: string): WebGLProgram {
+        const vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
         if (vertexShader === null) {
             throw new Error('Erro ao criar shader');
         }
 
-        this._gl.shaderSource(vertexShader, vSource);
-        this._gl.compileShader(vertexShader);
+        this.gl.shaderSource(vertexShader, vSource);
+        this.gl.compileShader(vertexShader);
 
         // checa erros de compilação
-        if (!this._gl.getShaderParameter(vertexShader, this._gl.COMPILE_STATUS)) {
-            const mensagem = this._gl.getShaderInfoLog(vertexShader);
+        if (!this.gl.getShaderParameter(vertexShader, this.gl.COMPILE_STATUS)) {
+            const mensagem = this.gl.getShaderInfoLog(vertexShader);
 
             if (mensagem !== null) {
                 throw new Error(mensagem);
             }
         }
 
-        const fragmentShader = this._gl.createShader(this._gl.FRAGMENT_SHADER);
+        const fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
         if (fragmentShader === null) {
             throw new Error('Erro ao criar shader');
         }
         
-        this._gl.shaderSource(fragmentShader, fSource);
-        this._gl.compileShader(fragmentShader);
+        this.gl.shaderSource(fragmentShader, fSource);
+        this.gl.compileShader(fragmentShader);
 
         // checa erros de compilação
-        if (!this._gl.getShaderParameter(fragmentShader, this._gl.COMPILE_STATUS)) {
-            const mensagem = this._gl.getShaderInfoLog(fragmentShader);
+        if (!this.gl.getShaderParameter(fragmentShader, this.gl.COMPILE_STATUS)) {
+            const mensagem = this.gl.getShaderInfoLog(fragmentShader);
 
             if (mensagem !== null) {
                 throw new Error(mensagem);
             }
         }
 
-        const programa = this._gl.createProgram();
+        const programa = this.gl.createProgram();
         if (programa === null) {
             throw new Error('Erro ao criar programa');
         }
 
-        this._gl.attachShader(programa, vertexShader);
-        this._gl.attachShader(programa, fragmentShader);
-        this._gl.linkProgram(programa);
+        this.gl.attachShader(programa, vertexShader);
+        this.gl.attachShader(programa, fragmentShader);
+        this.gl.linkProgram(programa);
       
         // checa erros de linkagem
-        if (!this._gl.getProgramParameter(programa, this._gl.LINK_STATUS)) {
-            const mensagem = this._gl.getShaderInfoLog(vertexShader);
+        if (!this.gl.getProgramParameter(programa, this.gl.LINK_STATUS)) {
+            const mensagem = this.gl.getShaderInfoLog(vertexShader);
 
             if (mensagem !== null) {
                 throw new Error(mensagem);
